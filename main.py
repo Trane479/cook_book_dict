275c803e29ec9763a8c2daace63bd0befc8a0888
Trane479 <gsevostyanov@gmail.com>
2020-04-03 22:58:12 +0300

    funcs added

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-
 from pprint import pprint
-
-
-# def txt_to_dict():
-#     cook_book_dict = {}
-#     while True:
-#         dish = cook_book.readline().strip()
-#         iteration_string = cook_book.readline()
-#
-#         i = 0
-#         cook_book_dict[dish] = []
-#         while i < int(iteration_string):
-#
-#             ingredient_string = (cook_book.readline().strip().split('|'))
-#             ingredient = ingredient_string[0]
-#             quantity = ingredient_string[1]
-#             measure = ingredient_string[2]
-#             cook_book_dict[dish].append({'ingredient name': ingredient, 'quantity': quantity, 'measure': measure})
-#             i += 1
-#         cook_book.readline()
-#         if not dish:
-#             break
-#     return cook_book_dict
 
 
 def txt_do_dict():
@@ -29,13 +6,12 @@ def txt_do_dict():
 
         cook_book_dict = {}
         while True:
-            dish = cook_book.readline().strip()  # Первая строка
-            iteration_string = cook_book.readline()  # Вторая строка
+            dish = cook_book.readline().strip()
+            iteration_string = cook_book.readline()
 
             i = 0
             cook_book_dict[dish] = []
-            while i < int(iteration_string):  # Цикл который читает n строк
-
+            while i < int(iteration_string):
                 ingredient_string = (cook_book.readline().strip().split('|'))
                 ingredient = ingredient_string[0]
                 quantity = ingredient_string[1]
@@ -43,21 +19,37 @@ def txt_do_dict():
                 cook_book_dict[dish].append({'ingredient name': ingredient, 'quantity': quantity, 'measure': measure})
                 i += 1
             cook_book.readline()
-            it = 'iteration'
             if i == 5:
                 break
-            return cook_book_dict
+    return cook_book_dict
+
+
+# pprint(txt_do_dict())
 
 
 def get_shop_list(dishes, person):
     cook_book_dict = txt_do_dict()
-    ingredient_to_buy = []
+    ingredients_to_buy = {}
     for dish in cook_book_dict:
         if dish in dishes:
             for ingredient in cook_book_dict[dish]:
-                ingredient['quantity'] = int(ingredient['quantity']) * person
-                print(ingredient)
+                # print(ingredient['ingredient name'])
 
-get_shop_list(['Омлет', 'Фахитос'], 2)
+                if ingredient['ingredient name'] not in ingredients_to_buy:
+                    ingredients_to_buy[ingredient['ingredient name']] = {'measure': ingredient['measure'],
+                                                                         'quantity': int(ingredient['quantity'])}
+                else:
+                    ingredients_to_buy[ingredient['ingredient name']] = {'measure': ingredient['measure'],
+                                                                         'quantity': int(ingredient['quantity']) +
+                                                                                     int(ingredient['quantity'])}
+    for ingredient in ingredients_to_buy:
+        ingredients_to_buy[ingredient]['quantity'] = ingredients_to_buy[ingredient]['quantity'] * person
+    return ingredients_to_buy
 
+
+def main():
+    pprint(get_shop_list(['Омлет', 'Фахитос'], 2))
+
+
+main()
 
